d0c4e529fabcd39b738527770a4e76a2e1a8d272
olewandowski1 <olewandowski@soldevelo.com>
2024-12-05 21:07:16 +0100

    build: fix index, add zustand as a peer

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,12 @@ import '@node_modules/@module/fe-core/src/styles/globals.css';
 
 import { TemplateComponent } from '@/components/template-component';
 
+// Main entry point for the module, include all configurations here
+const TEMPLATE_MODULE_CONFIG = {
+  contributionExample: ['TemplateComponent'],
+};
+
+// Export all components and utils to be used by other modules
 export { TemplateComponent };
+
+export default TEMPLATE_MODULE_CONFIG;
--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
           react: 'React',
           'react-dom': 'ReactDOM',
         },
+        exports: 'named',
       },
     },
     emptyOutDir: false,
