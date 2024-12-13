a6155de4279f966a1c0a0558ed6db8d7cb7dc162
olewandowski1 <olewandowski@soldevelo.com>
2024-12-13 22:46:54 +0100

    feat: export routes from module, adjust eslint rules

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import '@node_modules/@module/fe-core/src/styles/globals.css';
 
 import { TemplateComponent } from '@/components/template-component';
-import { Ref } from '@module/fe-core';
+import { Ref, Route, ROUTE_PATHS } from '@module/fe-core';
 
 // Main entry point for the module, include all configurations here
 const TEMPLATE_MODULE_CONFIG = {
@@ -12,6 +12,12 @@ const TEMPLATE_MODULE_CONFIG = {
       value: TemplateComponent,
     },
   ] as Ref[],
+  routes: [
+    {
+      path: ROUTE_PATHS.TEMPLATE,
+      component: TemplateComponent,
+    },
+  ] as Route[],
 };
 
 export default TEMPLATE_MODULE_CONFIG;
